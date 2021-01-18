5fceaeac33364099f46850798e18991240799fb3
Lawrence1196 <anonim1196@gmail.com>
2021-01-18 22:27:38 +0600

    Task2

--- LR/Main.py
+++ LR/Main.py
@@ -1,23 +1,29 @@
-import random
-
 from Warrior.Warrior import Warrior
+import random
+import copy
 
 if __name__ == "__main__":
-    warrior1 = Warrior('Воин1')
+    warrior1 = Warrior('Воин1',10)
     warrior2 = Warrior('Воин2')
-    warrior3 = Warrior('Воин3')
+    warrior3 = Warrior('Воин3',30)
     warriors = [warrior1, warrior2, warrior3]
+    copy_warriors = []
 
     while True:
         if len(warriors) == 1:
-            print(warriors[0].name + ' победил')
+            print('Победил %s' % str(warriors[0]))
             break
         x = random.choice(warriors)
         y = random.choice(warriors)
         if x != y:
             print('%s атакует %s' % (x.name, y.name))
-            y.health -= 20
+            y.health -= x.strength
             print('У %s осталось %s HP \n' % (y.name, y.health))
+            copy_warriors = copy.copy(warriors)
+            for i in range(len(copy_warriors)):
+                if (copy_warriors[i].health <= 0):
+                    warriors[i].__del__()
+                    warriors.pop(i)
             if y.health <= 0:
                 print('%s погиб от рук %s\n' % (x.name, y.name))
                 warriors.remove(x)
--- LR/Warrior/Warrior.py
+++ LR/Warrior/Warrior.py
@@ -1,4 +1,12 @@
 class Warrior:
-    def __init__(self, name, health = 100):
+    def __init__(self, name, strength=20, health=100):
         self.name = name
+        self.strength = strength
         self.health = health
+
+
+    def __str__(self):
+        return f"{self.name}, сила - {self.strength}, осталось здоровья - {self.health} HP"
+
+    def __del__(self):
+        print('Valhalla... %s is coming' % self.name)
